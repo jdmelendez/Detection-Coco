6dedfef66ec751461514d3c17c897335308bfd7e
Melendez95 <59265982+Melendez95@users.noreply.github.com>
2020-05-21 10:14:08 +0200

    Update coco_eval.py

--- references/detection/coco_eval.py
+++ references/detection/coco_eval.py
@@ -58,8 +58,6 @@ class CocoEvaluator(object):
         for iou_type, coco_eval in self.coco_eval.items():
             print("IoU metric: {}".format(iou_type))
             coco_eval.summarize()
-            
-
 
     def prepare(self, predictions, iou_type):
         if iou_type == "bbox":
